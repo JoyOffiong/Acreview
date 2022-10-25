165567ee10ac287a4e6db3fda3ba1425ef6f8b9f
JoyOffiong <offiongjoyasuquo@gmail.com>
2022-10-25 08:14:16 +0100

    more changes

--- main.js
+++ main.js
@@ -1,13 +1,14 @@
 const  menuBar=document.querySelector('.mobileheader__icons_navtoggle1');
 const  closeBar=document.querySelector('.mobileNav__navtoggle2');
 const  navBar=document.querySelector('.mobileNav');
-    // mainBody=document.getElementById('bodywrap')
+const mainBody=document.getElementById('bodywrap')
 
     menuBar.addEventListener('click', () =>{
    menuBar.style.display ="none"
    closeBar.style.display='block'
    closeBar.style.zIndex= '5'
     navBar.style.transform= 'translateX(0%)' 
+    mainBody.style.overflow='hidden'
  });
 
 closeBar.addEventListener('click', () =>{
